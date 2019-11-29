777c7874329efd61a7b1726d1c5c15f15ceaf58f
Kaliary Cisne de Sousa da Silva <kaliarycisne@gmail.com>
2019-11-28 21:43:21 -0300

    Api de departamentos

--- src/Controller/api/DepartamentoRestController.php
+++ src/Controller/api/DepartamentoRestController.php
@@ -7,10 +7,12 @@ namespace Root\Controller\api;
 
 use Root\Adapter\Connection;
 use Root\Entity\Department;
-use Root\Entity\User;
 
 final class DepartamentoRestController
 {
+    private $entityManager;
+    private $repository;
+
     public function __construct()
     {
         $this->entityManager = Connection::getEntityManager();
@@ -34,6 +36,64 @@ final class DepartamentoRestController
         echo json_encode($departments);
     }
 
-//
+    public function postAction(): void
+    {
+        $json = json_decode(file_get_contents('php://input'));
+
+        if (!isset($json->name)) {
+            echo json_encode([
+                'erro' => 'O atributo nome é obrigatório',
+            ]);
+            return;
+        }
+
+        $department = new Department();
+        $department->setName($json->name);
+        $department->setDescription($json->description ?? '');
+
+        $this->entityManager->persist($department);
+        $this->entityManager->flush();
+
+        echo json_encode($json);
+
+    }
+
+    public function putAction(): void
+    {
+        $json = json_decode(file_get_contents('php://input'));
+
+        $id = $_GET['id'];
+
+        try {
+            $department = $this->repository->find($id);
+            $department->setName($json->name ?? $department->getName());
+            $department->setDescription($json->description ?? $department->getDescription());
+
+            $this->entityManager->persist($department);
+            $this->entityManager->flush();
+            echo "Departamento atualizado!";
+        } catch (\Exception $e) {
+            echo "Departamento não atualizado!";
+        }
+    }
+
+    public function deleteAction(): void
+    {
+        try {
+            $id = $_GET['id'];
+
+            $department = $this->repository->find($id);
+
+            $this->entityManager->remove($department);
+            $this->entityManager->flush();
+
+//            echo "Departamento deletado!";
+            echo json_encode([
+                'success' => 'Departamento excluído',
+            ]);
+        } catch (\Exception $e) {
+            echo "Não foi possível deletar esse departamento!";
+        }
+    }
 
 }
